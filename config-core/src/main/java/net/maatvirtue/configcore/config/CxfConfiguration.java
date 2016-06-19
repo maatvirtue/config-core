9616d7318c1c4a5ad42cd55ebad8faef9327916f
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2016-06-19 00:00:23 -0400

    converted application to spring boot application

--- config-core/src/main/java/net/maatvirtue/configcore/config/CxfConfiguration.java
+++ config-core/src/main/java/net/maatvirtue/configcore/config/CxfConfiguration.java
@@ -1,4 +1,4 @@
-package net.maatvirtue.configcore.config.spring;
+package net.maatvirtue.configcore.config;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import net.maatvirtue.wsutils.filter.CorsFilter;
