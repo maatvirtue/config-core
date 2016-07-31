3fc21a14157284c81b412f56b7a5b51dc50a6422
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2016-07-30 20:16:44 -0400

    added service discovery and health check

--- src/main/java/net/maatvirtue/configcore/ConfigCoreApplication.java
+++ src/main/java/net/maatvirtue/configcore/ConfigCoreApplication.java
@@ -2,10 +2,12 @@ package net.maatvirtue.configcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigCoreApplication
 {
 	public static void main(String[] args)
