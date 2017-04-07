12366eda2e370088252e5be6e42f1ea34b0fa49e
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2017-04-06 21:39:39 -0400

    moved login to secret file

--- src/main/java/net/maatvirtue/configcore/ConfigCoreApplication.java
+++ src/main/java/net/maatvirtue/configcore/ConfigCoreApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
+@Configuration
+@PropertySource("classpath:/secret.properties")
 public class ConfigCoreApplication
 {
 	public static void main(String[] args)
