d1fb0ab95912beba274b8264b15cf5d99787cf82
Nicolas Lacombe <nicolas.m.lacombe@gmail.com>
2021-02-03 14:53:03 -0500

    - updated gradle wrapper, java, and dependencies - updated code to use external configuration

--- src/main/docker/startApp.sh
+++ src/main/docker/startApp.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-java -Djava.security.egd=file:/dev/./urandom -Dserver.port=${PORT} -Xms100m -Xmx280m -jar /app.jar
+command="java -Xms100m -Xmx280m"
+command+=" -Djava.security.egd=file:///dev/urandom"
+command+=" -Dspring.profiles.active=native"
+command+=" -Dspring.config.location=file:///app/config/application.yaml"
+command+=" -Dserver.port=${PORT}"
+command+=" -jar app.jar"
+
+echo "$command"
+$command
